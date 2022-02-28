46cd5cc6f5a85eb94c473d0375d611e942f49831
david promontor farias <davidgaston2341@gmail.com>
2022-02-28 16:53:37 -0600

    se crearon los archivos para el trabajo

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const { src, dest, watch , parallel } = require('gulp');
+const sass = require('gulp-dart-sass');
+const autoprefixer = require('autoprefixer');
+const postcss    = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
+const cssnano = require('cssnano');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const cache = require('gulp-cache');
+const webp = require('gulp-webp');
+
+const paths = {
+    scss: 'src/scss/**/*.scss',
+    imagenes: 'src/imagenesProyectos**/*'
+}
+
+// css es una función que se puede llamar automaticamente
+function css() {
+    return src(paths.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        // .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe( dest('./build/css') );
+}
+
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe(cache(imagemin({ optimizationLevel: 3})))
+        .pipe(dest('build/img'))
+        .pipe(notify({ message: 'Imagen Completada'}));
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe( webp() )
+        .pipe(dest('build/img'))
+        .pipe(notify({ message: 'Imagen Completada'}));
+}
+
+
+function watchArchivos() {
+    watch( paths.scss, css );
+    watch( paths.imagenes, imagenes );
+    watch( paths.imagenes, versionWebp );
+}
+  
+exports.default = parallel(css, versionWebp, watchArchivos ); 
